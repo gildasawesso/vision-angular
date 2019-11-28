deca19609d361b62cb42aaedee5f5e79becdb8f1
gildas awesso <gildas.dev@gmail.com>
2019-11-28 15:46:50 +0100

    fixed update student

--- src/app/core/constants/api.constants.ts
+++ src/app/core/constants/api.constants.ts
@@ -1,8 +1,8 @@
 import {environment} from '../../../environments/environment';
 
 export const apiConstants = {
-  // baseUrl: window['env'].apiUrl,
-  baseUrl: environment.baseUrl,
+  baseUrl: window['env'].apiUrl,
+  // baseUrl: environment.baseUrl,
   signin: '/auth/signin',
   signup: '/auth/signup'
 };
